033ba186d4f5fdaaa9c1d2fc0e667f21ec5c62cb
lifewithfernando <animofernando@gmail.com>
2023-03-06 01:37:27 +0900

    마지막 변경

--- result.js
+++ result.js
@@ -1,43 +1,32 @@
-import React, { useState, useEffect } from 'react';
-import xml2js from 'xml2js';
-
-const API_KEY = 'your_api_key_here';
-const API_URL = `https://your_firewall_ip_here/api/?type=config&action=get&xpath=/config/devices/entry[@name='localhost.localdomain']/vsys/entry[@name='vsys1']/rulebase/security`;
+import React, { useState, useEffect } from "react";
+import xml2js from "xml2js";
 
 const PolicyList = () => {
   const [policies, setPolicies] = useState([]);
 
   useEffect(() => {
-    const fetchData = async () => {
+    const fetchPolicies = async () => {
       try {
-        const response = await fetch(API_URL, {
-          headers: {
-            'Content-Type': 'application/xml',
-            'X-PAN-KEY': API_KEY
-          }
-        });
+        const response = await fetch(
+          "https://localhost/api/?type=config&action=get&xpath=/config/devices/entry[@name='localhost.localdomain']/vsys/entry[@name='vsys1']/rulebase"
+        );
         const data = await response.text();
         const parsedData = await xml2js.parseStringPromise(data);
-        const policyData = parsedData.response.result[0].rules[0].rule;
+        const policyData = parsedData.response.result[0].rules[0].entry;
         setPolicies(policyData);
       } catch (error) {
         console.log(error);
       }
     };
-    fetchData();
+
+    fetchPolicies();
   }, []);
 
   return (
     <div>
-      <h2>Policy List</h2>
-      {policies.map((policy, index) => (
-        <div key={index}>
-          <h3>Name: {policy.$.name}</h3>
-          <p>Source: {policy.from[0].member.join(', ')}</p>
-          <p>Destination: {policy.to[0].member.join(', ')}</p>
-          <p>Service: {policy.service[0].member.join(', ')}</p>
-          <p>Action: {policy.action[0]}</p>
-        </div>
+      <h1>Policy List</h1>
+      {policies.map((policy) => (
+        <div key={policy.$.name}>{policy.$.name}</div>
       ))}
     </div>
   );
